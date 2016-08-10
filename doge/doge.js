041d6fa2687cd7a490b9fe91adc0adadaaf554c0
Aiden Clarke <aidinio64@gmail.com>
2016-08-10 11:45:36 -0400

    Update doge.js

--- doge/doge.js
+++ doge/doge.js
@@ -14,9 +14,22 @@ class LoadState {
     console.log("loading");
     game.load.image('background', 'assets/background.jpg');
   }
+  create() {
+    console.log("loaded");
+    this.state.start("Play");
+  }
+}
+
+class PlayState {
+  create() {
+    console.log("play state");
+    this.background = this.add.tileSprite(0,0,320,568,"background");
+    this.background.autoScroll(0,700);
+  }
 }
 
 var game = new Phaser.Game(320,568);
 game.state.add("Boot",BootState);
 game.state.add("Load",LoadState);
+game.state.add("Play",PlayState);
 game.state.start("Boot");
